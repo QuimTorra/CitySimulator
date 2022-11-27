1eeeefbe2ad663e1743db4248df46a2d304ae49b
angelPrat <angel.prat@estudiantat.upc.edu>
2022-11-27 11:14:01 +0100

    landing page

---
+++ landing-Page/landingPage/src/App.svelte
@@ -0,0 +1,129 @@
+<script>
+
+</script>
+
+
+<div>
+  <!-- posar desto de de github com a clicable -->
+  <!-- titol -->
+  <div class="titleBar">
+    <a href="https://github.com/QuimTorra/TCSim"><div class="logoSource"></div></a>
+  </div>
+  
+  
+  <div class="apartats">
+    <h2>What is TC Simulator</h2>
+    <div class="contentP">
+      <p>Traffic Control Simulator is an open-source tool for simulating city traffic control, to get the optimal setup, to reduce waiting time for everyone, either car drivers or pedestrians.</p>
+    </div>
+  </div>
+
+  <div class="apartats">
+    <h2>Origen of the TC Simulator</h2>
+    <div class="contentP">
+      <p>This project was originally ideated and written in the </p>
+      <p href="https://unihack.eu/">[UniHack]</p>
+      <p>Hackathon, hosted in Timișoara, Romania, from the 24th to the 27th of November. </p>
+    </div>
+    
+    <h3>Track</h3>
+    <div class="contentP">
+      <p>We chose to develop over the Smart City, trying to find the best eco-friendly solution that we could achieve in a short ammount of time.
+        The first problem we found is that, since we're not from Timișoara, nor from anywhere near, seeing the problems in a city, or how it could be improved becomes sort of a challenge. 
+        
+        That's how we came up with this project idea: Traffic Control Simulator.</p>
+    </div>
+  </div>
+  
+  <div class="apartats">
+    <h2>Project</h2> 
+    <div class="contentP">
+      <p>We used mainly C++ to develop the software, since it's a programming language we're pretty familiar with.
+        Although it's a challenge to write a full Desktop Application in 48 hours using C++ as our language, we were confident in that we could take it on, and have at least a smal MVP for the project showcase, as a prove of concept.</p> 
+    </div>
+    <h3>Idea</h3>
+    <div class="contentP">
+      <p>The main point of this is to create an open-source Simulation Tool, based on cities, to be able to represent traffic situations, and how this could be solved.</p>  
+    </div>
+  </div>
+
+  <div class="apartats">
+    <h2>Developers</h2>
+    <div class="contentP">
+      <p>
+        This entire project was developed by us, comming from Barcelona, under the name of Aburrits.dev. 
+        This are the names of all 4 of the developers:
+      </p>  
+      <p>- Joaquim Torra Garcia </p>
+      <p>- Pau Gibert Moreno </p>
+      <p>- Àngel Prat Vilà </p>
+      <p>- Jorge Borrás i Duarte</p>
+    </div>
+  </div>
+  
+</div>
+
+
+<style>
+
+  .titleBar {
+    background-repeat: repeat-y;
+    background-image: url(TC_sim_long.png);
+    background-position: center;
+    background-color: #e6e6e6;
+    height: 275px;
+    width: auto;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    margin-bottom: 50px;
+  }
+
+  .logoSource{
+    background-position: center;
+    background-repeat: repeat-y;
+    background-image: url(github.png);
+    background-size: 50px;
+    height: 50px;
+    width: 50px;
+    position: absolute;
+    right: 20px;
+    top: 260px;
+  }
+
+  .contentP{
+    background-color: #e6e6e6;
+    padding: 20px;
+  }
+
+  .apartats{
+    margin-bottom: 20px;
+  }
+
+  h2{
+    background-color: #e11414;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 15px;
+    margin: 0px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+
+  h3{
+    margin: 0px;
+    background-color: #e1b314;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 20px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+
+  p{
+    margin: 0px;
+    background-color: white;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 25px;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+
+</style>
